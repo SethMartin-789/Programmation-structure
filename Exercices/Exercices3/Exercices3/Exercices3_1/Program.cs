608bf948ab06c27637a57720353071b822ba1d12
SethMartin-789 <2533376@etu.cchic.ca>
2025-09-05 11:42:16 -0400

    Nettoyage de solution

--- Exercices/Exercices3/Exercices3/Exercices3_1/Program.cs
+++ Exercices/Exercices3/Exercices3/Exercices3_1/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("Saisir la température en Fahrenheit et appuyer sur la touche Entrée :");
             fahrenheit = double.Parse(Console.ReadLine() ?? "0");
             celsius = (fahrenheit - 32) * 5 / 9;
-
+             
             Console.WriteLine($"La température correspondante en celsius est {celsius:F2} \u00B0C");
         }
     }
